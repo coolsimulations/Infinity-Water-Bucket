9226410dfd5f8f561db9b472da39335201ec7514
alkyaly <adriano.yt624@gmail.com>
2020-12-16 02:13:09 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Infinity-Water-Bucket
+
+Small fabric mixins mod that allows the enchantment Infinity for Water Buckets using an anvil, providing an infinite portable water source.
+
+Fabric port for the Infinity Water Bucket feature from [Quark](https://www.curseforge.com/minecraft/mc-mods/quark).
---
+++ src/main/java/com/github/alkyaly/infinitywaterbucket/mixin/BucketItemMixin.java
@@ -0,0 +1,29 @@
+package com.github.alkyaly.infinitywaterbucket.mixin;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BucketItem.class)
+public class BucketItemMixin extends Item {
+
+    public BucketItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getEmptiedStack", cancellable = true)
+    private void getEmptiedStack(ItemStack stack, PlayerEntity player, CallbackInfoReturnable<ItemStack> info) {
+        if(player.abilities.creativeMode || (EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0 && stack.getItem() == Items.WATER_BUCKET)) {
+            info.setReturnValue(stack);
+        } else info.setReturnValue(new ItemStack(Items.BUCKET));
+    }
+}
+
