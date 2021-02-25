47409e93b5a6485d9493fc702fee55cbcee28efd
alkyaly <adriano.yt624@gmail.com>
2021-02-25 06:05:40 -0300

    Was told this is better than the instaceof check

--- src/main/java/io/github/alkyaly/infinitywaterbucket/mixin/InfinityEnchantmentMixin.java
+++ src/main/java/io/github/alkyaly/infinitywaterbucket/mixin/InfinityEnchantmentMixin.java
@@ -1,17 +1,16 @@
 package io.github.alkyaly.infinitywaterbucket.mixin;
 
-import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.InfinityEnchantment;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Enchantment.class)
-public class EnchantmentMixin {
+@Mixin(InfinityEnchantment.class)
+public class InfinityEnchantmentMixin extends EnchantmentMixin{
 
-    @Inject(at = @At("HEAD"), method = "isAcceptableItem", cancellable = true)
+    @Override
     protected void stub(ItemStack stack, CallbackInfoReturnable<Boolean> info) {
+        info.setReturnValue(stack.getItem() == Items.WATER_BUCKET);
     }
-
 }
