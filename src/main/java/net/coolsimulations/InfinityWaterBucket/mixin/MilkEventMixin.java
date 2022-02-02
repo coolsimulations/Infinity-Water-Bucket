95ca48ce3391037f9827797f93dd2cb3449bff08
coolsimulations <coolsimulations@gmail.com>
2022-02-02 10:59:37 +1000

    Added Compatibility with Milk All The Mobs

---
+++ src/main/java/net/coolsimulations/InfinityWaterBucket/mixin/MilkEventMixin.java
@@ -0,0 +1,29 @@
+package net.coolsimulations.InfinityWaterBucket.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+
+@Mixin(targets = "com.natamus.milkallthemobs.events.MilkEvent", remap = false)
+public class MilkEventMixin {
+
+	@Inject(at = @At("HEAD"), method = "onEntityInteract", cancellable = true, require = 0)
+	private static void iwb$preventMilking(Player player, Level world, InteractionHand hand, Entity target, EntityHitResult hitResult, CallbackInfoReturnable<InteractionResult> info) {
+		ItemStack stack = player.getItemInHand(hand);
+		if(EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, stack) > 0 && stack.is(Items.BUCKET) && !world.isClientSide) {
+			info.setReturnValue(InteractionResult.PASS);
+		}
+	}
+}
