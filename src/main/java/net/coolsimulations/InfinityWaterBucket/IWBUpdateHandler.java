70eac01d8f27fc365a0953100a3502378bf48208
coolsimulations <coolsimulations@gmail.com>
2022-02-04 16:50:17 +1000

    Backport to 1.11.2

--- src/main/java/net/coolsimulations/InfinityWaterBucket/IWBUpdateHandler.java
+++ src/main/java/net/coolsimulations/InfinityWaterBucket/IWBUpdateHandler.java
@@ -21,7 +21,7 @@ public class IWBUpdateHandler {
 	public static void init() {
 		
 		try {
-            URL url = new URL("https://coolsimulations.net/mcmods/infinity-water-bucket/versionchecker112.txt");
+            URL url = new URL("https://coolsimulations.net/mcmods/infinity-water-bucket/versionchecker111.txt");
             Scanner s = new Scanner(url.openStream());
             latestVersion = s.next();
             s.close();
@@ -30,7 +30,7 @@ public class IWBUpdateHandler {
         }
 		
 		try {
-			URL url = new URL("https://coolsimulations.net/mcmods/infinity-water-bucket/updateinfo112.txt");
+			URL url = new URL("https://coolsimulations.net/mcmods/infinity-water-bucket/updateinfo111.txt");
 			Scanner s = new Scanner(url.openStream());
 			latestVersionInfo = s.nextLine();
 			s.close();
--- src/main/java/net/coolsimulations/InfinityWaterBucket/InfinityWaterBucket.java
+++ src/main/java/net/coolsimulations/InfinityWaterBucket/InfinityWaterBucket.java
@@ -1,20 +1,26 @@
 package net.coolsimulations.InfinityWaterBucket;
 
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
 @Mod(modid = IWBReference.MOD_ID, name = IWBReference.MOD_NAME, version = IWBReference.VERSION, acceptedMinecraftVersions = IWBReference.ACCEPTED_VERSIONS, dependencies = IWBReference.DEPENDENCIES, acceptableRemoteVersions = "*", updateJSON = "https://coolsimulations.net/mcmods/infinity-water-bucket/versionchecker.json")
 public class InfinityWaterBucket {
-
-	public InfinityWaterBucket() {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
 		IWBUpdateHandler.init();
 		IWBDispenserItemBehavior.init();
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	@SubscribeEvent
-	public static void onPlayerLogin(PlayerLoggedInEvent event)
+	public void onPlayerLogin(PlayerLoggedInEvent event)
 	{
 		if(IWBUpdateHandler.isOld == true && event.player instanceof EntityPlayerMP) {
 			if(event.player.getServer().isDedicatedServer()) {
