7bb6930dabf93479f2343912eaba13c57cfd5c6b
coolsim <coolsim@coolsimulations.net>
2025-12-21 17:31:14 +1000

    Update to 1.21.5

--- Common/src/main/java/net/coolsimulations/InfinityWaterBucket/mixin/DispenseItemBehavior5Mixin.java
+++ Common/src/main/java/net/coolsimulations/InfinityWaterBucket/mixin/DispenseItemBehavior5Mixin.java
@@ -3,30 +3,41 @@ package net.coolsimulations.InfinityWaterBucket.mixin;
 import net.coolsimulations.InfinityWaterBucket.InfinityWaterBucketCommon;
 import net.minecraft.core.dispenser.BlockSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BucketPickup;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 @Mixin(targets = "net/minecraft/core/dispenser/DispenseItemBehavior$5")
-public class DispenseItemBehavior5Mixin {
+public abstract class DispenseItemBehavior5Mixin extends DefaultDispenseItemBehavior {
 
-    @Unique
-    private ItemStack iwb$bucket;
+    @Inject(at = @At(value = "HEAD", ordinal = 0), method = "execute", cancellable = true)
+    private void iwb$getEmptyBucket(BlockSource blockSource, ItemStack itemStack, CallbackInfoReturnable<ItemStack> cir) {
+        if (InfinityWaterBucketCommon.hasInfinity(itemStack) && itemStack.is(Items.BUCKET)) {
+            ServerLevel serverLevel = blockSource.level();
+            BlockPos blockPos = blockSource.pos().relative(blockSource.state().getValue(DispenserBlock.FACING));
+            BlockState blockState = serverLevel.getBlockState(blockPos);
+            Block block = blockState.getBlock();
 
-    @Inject(method = "execute", at = @At("HEAD"))
-    public void getBucketItem(BlockSource source, ItemStack stack, CallbackInfoReturnable<ItemStack> info) {
-        iwb$bucket = stack.copy();
-    }
-
-    @Inject(at = @At(value = "RETURN", ordinal = 0), method = "execute", cancellable = true)
-    private void iwb$modifyWaterBucketBehavior(BlockSource source, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
-        if (InfinityWaterBucketCommon.hasInfinity(iwb$bucket) && (iwb$bucket.is(Items.WATER_BUCKET) ||
-                (iwb$bucket.is(Items.LAVA_BUCKET) && InfinityWaterBucketCommon.CONFIG.getInfiniteLavaBucket()) ||
-                InfinityWaterBucketCommon.isSolidBucket(iwb$bucket.getItem())))
-            cir.setReturnValue(iwb$bucket);
+            if (block instanceof BucketPickup) {
+                ItemStack itemStack2 = ((BucketPickup)block).pickupBlock(null, serverLevel, blockPos, blockState);
+                if (itemStack2.isEmpty())
+                    cir.setReturnValue(super.execute(blockSource, itemStack));
+                serverLevel.gameEvent(null, GameEvent.FLUID_PICKUP, blockPos);
+            } else {
+                cir.setReturnValue(super.execute(blockSource, itemStack));
+            }
+            cir.setReturnValue(itemStack);
+        }
     }
 }
