b9d08223adfe8dba0c8d8bcc58862540fc4b2f34
coolsim <coolsim@coolsimulations.net>
2025-12-21 17:57:39 +1000

    Update to 1.21.6

--- Forge/src/main/java/net/coolsimulations/InfinityWaterBucket/InfinityWaterBucket.java
+++ Forge/src/main/java/net/coolsimulations/InfinityWaterBucket/InfinityWaterBucket.java
@@ -1,7 +1,7 @@
 package net.coolsimulations.InfinityWaterBucket;
 
 import net.coolsimulations.InfinityWaterBucket.config.IWBConfig;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
